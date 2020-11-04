03af5e2518e5e48c77dabdd8c346995359629e5c
Louka Soret <louka.soret@gmail.com>
2020-11-04 11:04:01 +0100

    step counter

--- app/src/main/java/fr/frars/ikehm/MainActivity.java
+++ app/src/main/java/fr/frars/ikehm/MainActivity.java
@@ -2,15 +2,48 @@ package fr.frars.ikehm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.hardware.TriggerEventListener;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor stepsSensor;
+    private TriggerEventListener triggerEventListener;
+
+    private TextView stepTextView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        stepTextView = (TextView) findViewById(R.id.textView8);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        stepsSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+
+        if (stepsSensor == null) {
+            Toast.makeText(this, "No Step Counter Sensor !", Toast.LENGTH_SHORT).show();
+        } else {
+            sensorManager.registerListener(this, stepsSensor, SensorManager.SENSOR_DELAY_UI);
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        stepTextView.setText("" + event.values[0]);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
 }

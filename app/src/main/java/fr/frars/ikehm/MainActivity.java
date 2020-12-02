a6d1cc068df079d3d1b161eb6bc1a633ac1f8b78
Louka Soret <louka.soret@gmail.com>
2020-12-02 10:01:35 +0100

    speech recognition

--- app/src/main/java/fr/frars/ikehm/MainActivity.java
+++ app/src/main/java/fr/frars/ikehm/MainActivity.java
@@ -2,8 +2,12 @@ package fr.frars.ikehm;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -13,10 +17,17 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.speech.tts.TextToSpeech;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
@@ -39,6 +50,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private ProgressBar progressBar;
     private TextView progressTextView;
 
+    private TextToSpeech textToSpeech;
+
+    public static final Integer RecordAudioRequestCode = 1;
+    private SpeechRecognizer speechRecognizer;
+
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,17 +70,109 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
 
+
+
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    textToSpeech.setLanguage(Locale.UK);
+                }
+            }
+        });
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
+            checkPermission();
+        }
+
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);final Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.ENGLISH);
+
         runButton.setOnClickListener(v -> {
             if (runSession.isStarted()) {
                 runSession.stop();
                 user.globalStatistics.update(runSession.getTotalDistanceRan(), runSession.getTotalTimeRan());
                 runButton.setText(R.string.start_run);
+                speechRecognizer.startListening(speechRecognizerIntent);
             } else {
                 runSession.start();
                 runButton.setText(R.string.stop_run);
                 updateViews();
+                speechRecognizer.startListening(speechRecognizerIntent);
             }
         });
+
+        speechRecognizer.setRecognitionListener(new RecognitionListener(){
+
+            @Override
+            public void onReadyForSpeech(Bundle params) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+
+            }
+
+            @Override
+            public void onRmsChanged(float rmsdB) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] buffer) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+                speechRecognizer.startListening(speechRecognizerIntent);
+            }
+
+            @Override
+            public void onError(int error) {
+                speechRecognizer.startListening(speechRecognizerIntent);
+            }
+
+            @Override
+            public void onResults(Bundle results) {
+                ArrayList<String> data = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                if (runSession.isStarted() && data.get(0).contains("stop")) {
+                    runSession.stop();
+                    user.globalStatistics.update(runSession.getTotalDistanceRan(), runSession.getTotalTimeRan());
+                    runButton.setText(R.string.start_run);
+                    String toSpeak = "stopped";
+                    Toast.makeText(getApplicationContext(), toSpeak,Toast.LENGTH_SHORT).show();
+                    textToSpeech.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+                } else if(!runSession.isStarted() && data.get(0).contains("start")) {
+                    runSession.start();
+                    runButton.setText(R.string.stop_run);
+                    updateViews();
+                    String toSpeak = "started";
+                    Toast.makeText(getApplicationContext(), toSpeak,Toast.LENGTH_SHORT).show();
+                    textToSpeech.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+                }
+                speechRecognizer.startListening(speechRecognizerIntent);
+            }
+
+            @Override
+            public void onPartialResults(Bundle partialResults) {
+                speechRecognizer.startListening(speechRecognizerIntent);
+            }
+
+            @Override
+            public void onEvent(int eventType, Bundle params) {
+            }
+        });
+
+        speechRecognizer.startListening(speechRecognizerIntent);
+    }
+
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.RECORD_AUDIO},RecordAudioRequestCode);
+        }
     }
 
     @Override
